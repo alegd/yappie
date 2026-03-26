07644b2600700bd65a6470cb50432b14a5cf5dc4
ale.gd <ale.gueden@gmail.com>
2026-03-26 13:24:01 +0100

    feat: add GET /quotas endpoint via QuotasController
    YAP-101

--- apps/api/src/quotas/quotas.module.ts
+++ apps/api/src/quotas/quotas.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module } from "@nestjs/common";
 import { QuotasService } from "./quotas.service.js";
+import { QuotasController } from "./quotas.controller.js";
 import { AnalyticsModule } from "../analytics/analytics.module.js";
 
 @Global()
 @Module({
   imports: [AnalyticsModule],
+  controllers: [QuotasController],
   providers: [QuotasService],
   exports: [QuotasService],
 })
